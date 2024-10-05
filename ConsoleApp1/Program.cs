da25e475a2c2054c135fb268414cc40f09ed379d
David Eriksson <david.eriksson2@elev.ga.ntig.se>
2024-10-05 23:13:45 +0200

    progress

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -19,10 +19,11 @@ int i;
 string strCuantety;
 int intCuantety =0;
 float Cost =0;
-bool  Baugt = false;
+int  Baugt = 0;
 int Cheese =0;
 int Monkeys =0;
 int Sugar =0;
+bool unafordeble = false;
 
 SC.WriteLine(@$"
                             This is the store! Here you can spend your money on stupid things, 
@@ -46,12 +47,13 @@ Cuantity();
 
 void Items(){
     j=0;
+    Convertsucsses = false;
 // tab 7 text
     
 
     while (Convertsucsses == false) {
 
-if (Baugt == true){ SC.WriteLine(@$"Owned Cheese :{Cheese}:    Monkeys :{Monkeys}:     Sugar :{Sugar}:");}
+if (Baugt > 0){ SC.WriteLine(@$"Owned Cheese :{Cheese}:    Monkeys :{Monkeys}:     Sugar :{Sugar}:");}
 SC.WriteLine(@$"
 
                                                 Your Avaleble Money; {Money} kr
@@ -82,13 +84,6 @@ validAnswer = choices.Contains(item);
 if (validAnswer == false ) { Convertsucsses = false; }
 
 j++;
-/*
-if (Money < 17.5){
-SC.WriteLine("Your Unfortunatly broke, =/ .There fore we dont want you here anymore.");
-}
-*/
-
-
 
 
 SC.Clear();
@@ -101,115 +96,194 @@ SC.Clear();
 void Cuantity(){
 i = 1;
 j = 0;
-
+intCuantety = 0;
 while (i == 1){
     
 if (stockCheese == 0 && itemNum == 1){
-    SC.WriteLine("We have unfortunetly run out of inventory for that item, plese chose another one");
+    SC.WriteLine("We have unfortunetly run out of inventory for that item, plese chose another item");
     SC.ReadLine();
 itemNum = 0;
+i = 0;
 }
 
 if (stockMonkey == 0 && itemNum == 2){
-    SC.WriteLine("We have unfortunetly run out of inventory for that item, plese chose another one");
+    SC.WriteLine("We have unfortunetly run out of inventory for that item, plese chose another item");
     SC.ReadLine();
 itemNum = 0;
+i = 0;
 }
 
 if (stockSugar == 0 && itemNum == 3){
-    SC.WriteLine("We have unfortunetly run out of inventory for that item, plese chose another one");
+    SC.WriteLine("We have unfortunetly run out of inventory for that item, plese chose another item");
     SC.ReadLine();
 itemNum = 0;
+i = 0;
 }
 
 if (itemNum == 1){
 SC.WriteLine(@$"
                               How many Chesses do you want to buy? They cost: 17.45 kr ech.
                                                   We have {stockCheese} in stock.
-                                                Write how you want many:
+                                              Write how many you want many:
 "); 
 
 if (Convertsucsses == false && j > 0) {
     SC.WriteLine(@"
-                                          Only acepts numerical valius, try again");
+                                        We only acepts numerical valius, try again");
 } 
+
+if (intCuantety > stockCheese){
+SC.WriteLine(@"            
+            We unfortunelty dont have the recuiered inventory for your transaction plese chose a lower amount");
+}
+if (Convertsucsses == true && Money < Cost) {
+SC.WriteLine(@"            
+            You unfortunetly do not have the requierd funds for the transaction, please aquier aditenal funds");
+} 
+
 
 strCuantety = SC.ReadLine();
 Cost = 0;
 
 Convertsucsses = int.TryParse(strCuantety, out intCuantety); // försöker parsa om mislykas går vidare med 0
 
-Cost = 17.45f * intCuantety;
-Cheese = intCuantety; 
+Cost = 17.45f * intCuantety; 
 if (Convertsucsses == true && Money > Cost  && intCuantety <= stockCheese) {
 SC.WriteLine($@"
-                            You have now baugt {intCuantety} Chesses.");
+                                              You have now baugt {intCuantety} Chesses.");
                             SC.ReadLine();
 stockCheese = stockCheese - intCuantety;
+Cheese =+ intCuantety;
 i = 0;
 Money = Money-Cost;
 }
 
 }
 
-else if (itemNum == 2){
-    
 
+
+
+
+else if (itemNum == 2){
+
+SC.WriteLine(@$"
+                              How many Monkeys do you want to buy? They cost: 36.99 kr ech.
+                                                  We have {stockMonkey} in stock.
+                                              Write how many you want many:
+"); 
+
+if (Convertsucsses == false && j > 0) {
+    SC.WriteLine(@"
+                                        We only acepts numerical valius, try again");
+} 
+
+if (intCuantety > stockMonkey){
+SC.WriteLine(@"            
+            We unfortunelty dont have the recuiered inventory for your transaction plese chose a lower amount");
+}
+if (Convertsucsses == true && Money < Cost) {
+SC.WriteLine(@"            
+            You unfortunetly do not have the requierd funds for the transaction, please aquier aditenal funds");
+            unafordeble = true;
+            i = 0;
+            SC.ReadLine();
+} 
+
+if (unafordeble == false){
+strCuantety = SC.ReadLine();
+Cost = 0;
+
+Convertsucsses = int.TryParse(strCuantety, out intCuantety); // försöker parsa om mislykas går vidare med 0
+
+Cost = 36.99f * intCuantety; 
+if (Convertsucsses == true && Money > Cost  && intCuantety <= stockMonkey) {
+SC.WriteLine($@"
+                                              You have now baugt {intCuantety} Monkeys.");
+                            SC.ReadLine();
+stockMonkey = stockMonkey - intCuantety;
+Monkeys =+ intCuantety;
+i = 0;
+Money = Money-Cost;
+}
+
+
+} // if (unafordeble = false)
 } 
 
 
 else if (itemNum == 3){
 
-}
+SC.WriteLine(@$"
+                              How mutch Sugar do you want to buy? They cost: 69.69 kr per bag.
+                                                  We have {stockSugar} in stock.
+                                              Write how many you want many:
+"); 
 
+if (Convertsucsses == false && j > 0) {
+    SC.WriteLine(@"
+                                        We only acepts numerical valius, try again");
+} 
 
-if (intCuantety > stockCheese){
-    SC.WriteLine("We unfortunelty dont have the recuiered inventory for your transaction plese chose a lower amount");
+if (intCuantety > stockSugar){
+SC.WriteLine(@"            
+            We unfortunelty dont have the recuiered inventory for your transaction plese chose a lower amount");
 }
 if (Convertsucsses == true && Money < Cost) {
-SC.WriteLine("You unfortunetly do not have the requierd funds for the transaction, please aquier aditenal funds");
-SC.ReadLine();
+SC.WriteLine(@"            
+            You unfortunetly do not have the requierd funds for the transaction, please aquier aditenal funds");
 } 
+
+
+strCuantety = SC.ReadLine();
+Cost = 0;
+
+Convertsucsses = int.TryParse(strCuantety, out intCuantety); // försöker parsa om mislykas går vidare med 0
+
+Cost = 17.45f * intCuantety; 
+if (Convertsucsses == true && Money > Cost  && intCuantety <= stockSugar) {
+SC.WriteLine($@"
+                                              You have now baugt {intCuantety} bags of Sugar.");
+                            SC.ReadLine();
+stockSugar = stockSugar - intCuantety;
+Sugar =+ intCuantety;
+i = 0;
+Money = Money-Cost;
+}
+
+}
+
+
+
 
 
 //outPut();
 j++;
-Baugt = true;
-Convertsucsses = false;
+//Convertsucsses = false;
 SC.Clear();
-} // while (i == 1)
 
+} // while (i == 1)
+//if (unafordeble = false){
+Baugt++;
+//}
+
+    if (Money < 17.5){
+SC.WriteLine("Your Unfortunatly broke, =/ .Therefore we dont want you here anymore.");
+SC.ReadLine();
+Running = false;
+}
 
 } // Cuantety
 
 
 
+while(true){
+    SC.Clear();
+    SC.ReadLine();
+    SC.WriteLine(@"
 
-// for (int i = 0; i < 10; i++)
-// {
-//     SC.WriteLine("Hej");
-// }
-
-// SC.WriteLine("Skriv in din older"); 
-// string age = Console.ReadLine();
-
-// int ageNum = 0;
-// bool sucsses = false;
-
-// while (sucsses == false) {
-// sucsses = int.TryParse(age, out ageNum); // försöker parsa om mislykas går vidare med 0
-
-// if ( sucsses == false) {
-//     SC.WriteLine("No, a numer, idot!");
-// }
-// }
-
-// //ageNum = int.Parse(age); // stänger pogramet om fel
-
-// age = ageNum.ToString();
-
-// if (ageNum > 18) 
-// {
-// SC.WriteLine("Welcome!"); 
-// }
-
+    
+    bot
+    
+    
+    ");
+}
